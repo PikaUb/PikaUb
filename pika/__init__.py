2f2c5a5c6ac0ec1bc3a49215b34bb8f135da5997
Yumaka Lukaśz <128088905+yumakameow@users.noreply.github.com>
2023-09-02 01:09:01 +0500

    Create __init__.py

---
+++ pika/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Yumaka Lukasz"
+__contact__ = "yumaka.dev@gmail.com"
+__copyright__ = "Copyright 2023, Yumakameow Lukasz"
+__credits__ = ["LonamiWebs", "penn5"]
+__license__ = "AGPLv3"
+__maintainer__ = "developer"
+__status__ = "Production"
