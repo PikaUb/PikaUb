8b8751fa3da2c5e985b20829d5e39c1144ba6536
Yumaka Lukaśz <128088905+yumakameow@users.noreply.github.com>
2023-09-02 10:38:26 +0500

    Create version.py

---
+++ pika/version.py
@@ -0,0 +1,12 @@
+__version__ = (1, 6, 1)
+netver = (0, 3, 3)
+netrev = ""
+import os
+import git
+
+try:
+    branch = git.Repo(
+        path=os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    ).active_branch.name
+except Exception:
+    branch = "master"
